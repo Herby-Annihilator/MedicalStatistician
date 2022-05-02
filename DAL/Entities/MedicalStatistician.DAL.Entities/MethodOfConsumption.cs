cdc669e4c9e214183b1a68ef0aadbcc72a5196e8
Rukin Danil <rukin2018d@yandex.ru>
2022-05-02 17:01:23 +0700

    способ употребления ПАВ

---
+++ DAL/Entities/MedicalStatistician.DAL.Entities/MethodOfConsumption.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalStatistician.DAL.Entities
+{
+    /// <summary>
+    /// Способ употребления ПАВ
+    /// </summary>
+    public class MethodOfConsumption
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        /// <summary>
+        /// Формулировка
+        /// </summary>
+        [MaxLength(100)]
+        [Required]
+        public string Wording { get; set; } = "";
+    }
+}
