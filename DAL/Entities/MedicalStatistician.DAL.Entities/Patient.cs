e23dd5001cc7ec627c9e4361febbaa221e115971
Rukin Danil <rukin2018d@yandex.ru>
2022-05-02 14:36:57 +0700

    пациент

---
+++ DAL/Entities/MedicalStatistician.DAL.Entities/Patient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalStatistician.DAL.Entities
+{
+    /// <summary>
+    /// Пациент
+    /// </summary>
+    public class Patient
+    {
+        /// <summary>
+        /// Первичный ключ
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Имя (макс. длина 300)
+        /// </summary>
+        [MaxLength(300)]
+        public string FirstName { get; set; } = "";
+
+        /// <summary>
+        /// Фамилия (макс. длина 300)
+        /// </summary>
+        [MaxLength(300)]
+        public string LastName { get; set; } = "";
+
+        /// <summary>
+        /// Отчество (макс. длина 300). Необязательное свойство
+        /// </summary>
+        [MaxLength(300)]
+        public string? Patronymic { get; set; }
+
+        /// <summary>
+        /// День рождения
+        /// </summary>
+        public DateTime Birthday { get; set; }
+        /// <summary>
+        /// Признак использования чужих шприцов
+        /// </summary>
+        public bool IsUsedOthersSyringes { get; set; } = false;
+        /// <summary>
+        /// Признак проживания с наркоманом
+        /// </summary>
+        public bool IsLivesWithAddict { get; set; } = false;
+    }
+}
