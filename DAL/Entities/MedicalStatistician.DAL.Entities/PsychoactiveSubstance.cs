66b70a35e09d987f2673c4acf0162bb0848520f4
Rukin Danil <rukin2018d@yandex.ru>
2022-05-02 17:12:23 +0700

    ПАВ

---
+++ DAL/Entities/MedicalStatistician.DAL.Entities/PsychoactiveSubstance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalStatistician.DAL.Entities
+{
+    /// <summary>
+    /// Психоактивное вещество (ПАВ)
+    /// </summary>
+    public class PsychoactiveSubstance
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        /// <summary>
+        /// Название психоактивного вещества
+        /// </summary>
+        [MaxLength(512)]
+        public string Name { get; set; } = "";
+    }
+}
