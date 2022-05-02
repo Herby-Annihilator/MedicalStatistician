b5cd7ea014ef026ce99dbcb0f205d2d6f12964da
Rukin Danil <rukin2018d@yandex.ru>
2022-05-03 00:36:30 +0700

    вид принудительного лечения

---
+++ DAL/Entities/MedicalStatistician.DAL.Entities/TypeOfForcedTreatment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalStatistician.DAL.Entities
+{
+    /// <summary>
+    /// Вид принудительного лечения
+    /// </summary>
+    public class TypeOfForcedTreatment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        /// <summary>
+        /// Формулировка
+        /// </summary>
+        [MaxLength(1024)]
+        public string Wording { get; set; } = "";
+    }
+}
