94daf81993a99f355d983c61123cc7089b35c233
Rukin Danil <rukin2018d@yandex.ru>
2022-05-02 18:54:43 +0700

    оквэд2

--- DAL/Entities/MedicalStatistician.DAL.Entities/OKATO.cs
+++ DAL/Entities/MedicalStatistician.DAL.Entities/OKATO.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 
 namespace MedicalStatistician.DAL.Entities
 {
+    /// <summary>
+    /// Справочник ОКАТО
+    /// </summary>
     public class Okato
     {
         [Key]
---
+++ DAL/Entities/MedicalStatistician.DAL.Entities/Okved2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalStatistician.DAL.Entities
+{
+    /// <summary>
+    /// ОКВЭД2
+    /// </summary>
+    public class Okved2
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// Раздел общероссийского классификатора видов экономической деятельности
+        /// </summary>
+        [MaxLength(1)]
+        public string SectionName { get; set; } = "A";
+        /// <summary>
+        /// Код по общероссийскому классификатору видов экономической деятельности
+        /// </summary>
+        [MaxLength(100)]
+        public string Code { get; set; } = "";
+        /// <summary>
+        /// Наименование по общероссийскому классификатору видов экономической деятельности
+        /// </summary>
+        [MaxLength(2048)]
+        public string Name { get; set; } = "";
+    }
+}
