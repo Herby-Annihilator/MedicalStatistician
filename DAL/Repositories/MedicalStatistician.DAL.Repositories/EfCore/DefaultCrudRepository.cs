017ed095162b2c6f94d6bf8e846c1aa9405584d0
Rukin Danil <rukin2018d@yandex.ru>
2022-05-27 21:05:45 +0700

    Стандартная реализация crud репозитория

--- DAL/Repositories/MedicalStatistician.DAL.Repositories/EfCore/DefaultCrudRepository.cs
+++ DAL/Repositories/MedicalStatistician.DAL.Repositories/EfCore/DefaultCrudRepository.cs
@@ -1,4 +1,7 @@
-﻿using MedicalStatistician.DAL.Repositories.Base;
+﻿using MedicalStatistician.DAL.Entities.Base;
+using MedicalStatistician.DAL.Entities.DbContexts;
+using MedicalStatistician.DAL.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +10,83 @@ using System.Threading.Tasks;
 
 namespace MedicalStatistician.DAL.Repositories.EfCore
 {
-    public class DefaultCrudRepository : ICrudRepository
+    /// <summary>
+    /// Стандартная реализация crud репозитория <see cref="ICrudRepository{T}"/>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <inheritdoc/>
+    public class DefaultCrudRepository<T> : ICrudRepository<T> where T : Entity
     {
+        protected MedicalStatisticianDbContext _context;
+        protected DbSet<T> EntitySet => _context.Set<T>();
+        public DefaultCrudRepository(MedicalStatisticianDbContext context)
+        {
+            _context = context;
+        }
+        public virtual int Create(T entity)
+        {
+            if (entity == null)
+                return 0;
+            var entry = EntitySet.Add(entity);
+            if (entry == null)
+                return 0;
+            return _context.SaveChanges();
+        }
+
+        public virtual async Task<int> CreateAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            int result = 0;
+            if (entity != null)
+            {
+                var entry = await EntitySet.AddAsync(entity, cancellationToken);
+                if (entry != null)
+                {
+                    result = await _context.SaveChangesAsync(cancellationToken);
+                }
+            }
+            return result;
+        }
+
+        public virtual int Delete(T entity)
+        {
+            if (entity == null)
+                return 0;
+            EntitySet.Remove(entity);
+            return _context.SaveChanges();
+        }
+
+        public virtual async Task<int> DeleteAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            if (entity == null)
+                return 0;
+            EntitySet.Remove(entity);
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public virtual IEnumerable<T> GetAll() => EntitySet.ToArray();
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default)
+            => await EntitySet.ToArrayAsync(cancellationToken);
+
+        public virtual T GetById(int id) => EntitySet.FirstOrDefault(entity => entity.Id == id);
+
+        public virtual async Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+            => await EntitySet.FirstOrDefaultAsync(entity => entity.Id == id, cancellationToken);
+
+        public virtual int Update(T entity)
+        {
+            if (entity == null)
+                return 0;
+            EntitySet.Update(entity);
+            return _context.SaveChanges();
+        }
+
+        public virtual async Task<int> UpdateAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            if (entity == null)
+                return 0;
+            EntitySet.Update(entity);
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
     }
 }
