3944e6c7062783b30dd02d72c63025fde6bac5fc
Rukin Danil <rukin2018d@yandex.ru>
2022-05-30 21:45:44 +0700

    контроллер для пола, регистрация репозиториев

---
+++ API/MedicalStatistician.API/Controllers/SexController.cs
@@ -0,0 +1,16 @@
+﻿using MedicalStatistician.API.Controllers.Base;
+using MedicalStatistician.DAL.Entities;
+using MedicalStatistician.DAL.Repositories.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalStatistician.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SexController : DefaultCrudController<Sex>
+    {
+        public SexController(ICrudRepository<Sex> repository) : base(repository)
+        {
+        }
+    }
+}
