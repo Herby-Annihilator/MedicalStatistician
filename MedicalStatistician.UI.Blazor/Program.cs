229150fc75f4b2829c50adef90fe011778f26eca
Fierce-Eagle <rukin_nikitka@mail.ru>
2022-06-03 13:20:11 +0700

    добавлен проброс данных на страницу simpleCatalog

--- MedicalStatistician.UI.Blazor/Program.cs
+++ MedicalStatistician.UI.Blazor/Program.cs
@@ -1,11 +1,27 @@
+using MedicalStatistician.DAL.Entities.Base;
+using MedicalStatistician.DAL.Entities.DbContexts;
+using MedicalStatistician.DAL.Repositories.Base;
+using MedicalStatistician.DAL.Repositories.EfCore;
 using MedicalStatistician.UI.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var services = builder.Services;
+services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+////
+builder.Services.AddDbContext<MedicalStatisticianDbContext>(
+    opt => opt
+    .UseNpgsql(
+        builder.Configuration.GetConnectionString("Postgres"),
+        b => b.MigrationsAssembly("MedicalStatistician.DAL.Postrgres"))
+);
+
+services.AddScoped(typeof(ICrudRepository<>), typeof(DefaultCrudRepository<>));
 
 await builder.Build().RunAsync();
