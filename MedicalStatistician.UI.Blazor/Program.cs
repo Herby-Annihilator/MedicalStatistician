2a21191146f48fa534036629a4853c1b3cdb24fc
Fierce-Eagle <rukin_nikitka@mail.ru>
2022-06-03 18:57:33 +0700

    кол-во ошибок сведено к одной большой ошибке

--- MedicalStatistician.UI.Blazor/Program.cs
+++ MedicalStatistician.UI.Blazor/Program.cs
@@ -1,8 +1,10 @@
+using MedicalStatistician.DAL.Entities;
 using MedicalStatistician.DAL.Entities.Base;
 using MedicalStatistician.DAL.Entities.DbContexts;
 using MedicalStatistician.DAL.Repositories.Base;
 using MedicalStatistician.DAL.Repositories.EfCore;
 using MedicalStatistician.UI.Blazor;
+using MedicalStatistician.WebApiClients.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -14,14 +16,35 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 var services = builder.Services;
 services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+
+services.AddHttpClient<ICrudRepository<Sex>, WebRepository<Sex>>(
+    (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/Sex"));
+/*services.AddHttpClient<ICrudRepository<NamedEntity>, WebRepository<NamedEntity>>(
+    (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/DisabilityGroup"));
+services.AddHttpClient<ICrudRepository<NamedEntity>, WebRepository<NamedEntity>>(
+    (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/PurposeOfReferralForTreatment"));
+services.AddHttpClient<ICrudRepository<NamedEntity>, WebRepository<NamedEntity>>(
+    (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/WhoSentToHospital"));*/
+//var http = new HttpClient()
+//{
+//    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+//};
+
+//builder.Services.AddScoped(sp => http);
+
+//var response = await http.GetAsync("appsettings.json");
+//var stream = await response.Content.ReadAsStreamAsync();
+
+//builder.Configuration.AddJsonStream(stream);
+
 ////
-builder.Services.AddDbContext<MedicalStatisticianDbContext>(
+/*builder.Services.AddDbContext<MedicalStatisticianDbContext>(
     opt => opt
     .UseNpgsql(
         builder.Configuration.GetConnectionString("Postgres"),
         b => b.MigrationsAssembly("MedicalStatistician.DAL.Postrgres"))
 );
 
-services.AddScoped(typeof(ICrudRepository<>), typeof(DefaultCrudRepository<>));
+services.AddScoped(typeof(ICrudRepository<>), typeof(DefaultCrudRepository<>));*/
 
 await builder.Build().RunAsync();
