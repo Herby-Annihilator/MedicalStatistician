8e727461397985f8740c688dcbe2d13fdab9088f
Rukin Danil <rukin2018d@yandex.ru>
2022-06-04 21:36:58 +0700

    Таблица 2200 "Состав больных, находящихся на принудительном лечении (ПЛ) в психиатрических стационарах"

--- Reports/MedicalStatistician.Reports/Exporters/Word/WordExporter.cs
+++ Reports/MedicalStatistician.Reports/Exporters/Word/WordExporter.cs
@@ -19,24 +19,6 @@ namespace MedicalStatistician.Reports.Exporters.Word
             throw new NotImplementedException();
         }
 
-        public void Export(string path, ActiveDispensaryObservation activeDispensaryObservation)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Export(string path, ReportTable2110 reportTable2110)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Export(string path, ReportTable2120 reportTable2120)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Export(string path, ReportTable2130 reportTable2130)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
