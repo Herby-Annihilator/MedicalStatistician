01808151b156fde8c4a754859da3578a0f3f217f
Fierce-Eagle <rukin_nikitka@mail.ru>
2022-06-05 23:13:42 +0700

    добавлены справочники кроме образования и МКБ

--- MedicalStatistician.UI.Blazor/Program.cs
+++ MedicalStatistician.UI.Blazor/Program.cs
@@ -37,6 +37,35 @@ namespace MedicalStatistician.UI.Blazor
                  (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/PurposeOfReferralForTreatment"));
             services.AddHttpClient<ICrudRepository<WhoSentToHospital>, WebRepository<WhoSentToHospital>>(
                 (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/WhoSentToHospital"));
+            services.AddHttpClient<ICrudRepository<SourceOfLivelihood>, WebRepository<SourceOfLivelihood>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/SourceOfLivelihood"));
+            services.AddHttpClient<ICrudRepository<DiseaseOutcome>, WebRepository<DiseaseOutcome>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/DiseaseOutcome"));
+            services.AddHttpClient<ICrudRepository<CauseOfDeath>, WebRepository<CauseOfDeath>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/CauseOfDeath"));
+            services.AddHttpClient<ICrudRepository<Education>, WebRepository<Education>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/Education"));
+            services.AddHttpClient<ICrudRepository<ResidenceStatus>, WebRepository<ResidenceStatus>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/ResidenceStatus"));
+            services.AddHttpClient<ICrudRepository<TypeOfForcedTreatment>, WebRepository<TypeOfForcedTreatment>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/TypeOfForcedTreatment"));
+            services.AddHttpClient<ICrudRepository<PlaceOfDeparture>, WebRepository<PlaceOfDeparture>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/PlaceOfDeparture"));
+            services.AddHttpClient<ICrudRepository<TypeOfOutpatientCare>, WebRepository<TypeOfOutpatientCare>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/TypeOfOutpatientCare"));
+            services.AddHttpClient<ICrudRepository<OrderOfAdmission>, WebRepository<OrderOfAdmission>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/OrderOfAdmission"));
+            services.AddHttpClient<ICrudRepository<StatusOfJudgesDecisionUnderArticle35>, WebRepository<StatusOfJudgesDecisionUnderArticle35>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/StatusOfJudgesDecisionUnderArticle35"));
+            services.AddHttpClient<ICrudRepository<TypeOfJudgment>, WebRepository<TypeOfJudgment>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/TypeOfJudgment"));
+            services.AddHttpClient<ICrudRepository<PatientEntryRoutes>, WebRepository<PatientEntryRoutes>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/PatientEntryRoutes"));
+            services.AddHttpClient<ICrudRepository<Accommodations>, WebRepository<Accommodations>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/Accommodations"));
+            services.AddHttpClient<ICrudRepository<SourcesOfPaymentForMedicalCare>, WebRepository<SourcesOfPaymentForMedicalCare>>(
+                (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/SourcesOfPaymentForMedicalCare"));
+
         }
     }
 }
