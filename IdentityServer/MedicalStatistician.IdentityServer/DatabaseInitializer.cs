5bb0298700ac29a11b1637e4c08366de2f1e4143
Rukin Danil <rukin2018d@yandex.ru>
2022-06-14 21:25:49 +0700

    Добавление инициализации БД данными из конфига

---
+++ IdentityServer/MedicalStatistician.IdentityServer/DatabaseInitializer.cs
@@ -0,0 +1,46 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MedicalStatistician.IdentityServer
+{
+    public static class DatabaseInitializer
+    {
+        public static void Init(IServiceProvider scopeServiceProvider)
+        {
+            scopeServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+            var context = scopeServiceProvider.GetRequiredService<ConfigurationDbContext>();
+            context.Database.Migrate();
+            if (!context.Clients.Any())
+            {
+                foreach (var client in Config.Clients)
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                foreach (var resource in Config.IdentityResources)
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.ApiScopes.Any())
+            {
+                foreach (var resource in Config.ApiScopes)
+                {
+                    context.ApiScopes.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
