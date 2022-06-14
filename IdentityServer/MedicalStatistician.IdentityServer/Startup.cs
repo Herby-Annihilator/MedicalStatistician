8e6c6ec6304ed0d360be5ea8ae02c6f75dc0c0d7
Rukin Danil <rukin2018d@yandex.ru>
2022-06-14 17:29:52 +0700

    миграции для базы Identity

--- IdentityServer/MedicalStatistician.IdentityServer/Startup.cs
+++ IdentityServer/MedicalStatistician.IdentityServer/Startup.cs
@@ -1,13 +1,17 @@
+using AutoMapper.Configuration;
 using IdentityServer4.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace MedicalStatistician.IdentityServer
 {
@@ -15,12 +19,35 @@ namespace MedicalStatistician.IdentityServer
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        private readonly Microsoft.Extensions.Configuration.IConfiguration _configuration;
+
+        public Startup(Microsoft.Extensions.Configuration.IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            var migrationsAssembly = "MedicalStatistician.DAL.Postrgres";
             services.AddIdentityServer()
-                .AddInMemoryClients(Config.Clients)
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddDeveloperSigningCredential();
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                    {
+                        builder.UseNpgsql(_configuration.GetConnectionString("Identity"),
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                    };
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseNpgsql(_configuration.GetConnectionString("Identity"),
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 3600;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
